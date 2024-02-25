a635d36a08dbe7713eb88fd4dc77fa1fa8373ff8
NattaphatSuknakkit <6406021620033@fitm.kmutnb.ac.th>
2024-02-25 19:40:25 +0700

    update

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ const path = require('path')
 const app = express()
 var bodyParser = require('body-parser')
 
-
-
 const base_url = "http://localhost:3000"
 //const base_url = "http://10.104.7.149"
-//const base_url = "http://node56384-noderest-test1.proen.app.ruk-com.cloud"
+//const base_url = "http://node56420-noedrestfifu.proen.app.ruk-com.cloud"
 
 
 
